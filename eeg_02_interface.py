25af6a9ddaabe44044e4f7ea3c994590f551b475
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-04-21 22:54:30 +0200

    main script commit. Comments only.

---
+++ eeg_02_interface.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+'''
+eeg_02_interface.py
+
+Main script. Performs all the operations.
+
+After the ANN is trained it can be used to classify a package of data.
+
+1. The program starts.
+2. The package of data is obtained
+3. The package is read by the C script to classify it as blinking or not blinking.
+4. If 3 consecutive packages are classified as blinking then the picture is switched.
+'''
+
+
+
+
+
+
